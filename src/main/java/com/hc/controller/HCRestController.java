66b4828fdcd0edd0e14c1e85750f6ba068b186a2
Lakshmi Ramanathan <34840562+lakshmi-ram@users.noreply.github.com>
2018-04-07 16:50:31 +0530

    Update HCRestController.java

--- src/main/java/com/hc/controller/HCRestController.java
+++ src/main/java/com/hc/controller/HCRestController.java
@@ -1,24 +1,19 @@
 package com.hc.controller;
 
 import java.io.FileInputStream;
-import java.util.ArrayList;
-import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.poi.hssf.usermodel.HSSFRow;
-import org.apache.poi.hssf.usermodel.HSSFSheet;
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.poifs.filesystem.POIFSFileSystem;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 
 import com.hc.domain.HealthMetrics;
 import com.hc.dto.IllnessCountResp;
@@ -48,32 +43,32 @@ public class HCRestController {
 	@RequestMapping(value = "/process-excel", method = RequestMethod.POST)
 	public String processExcel(String fileName){
 		try {
-			int i = 0;
-			fileName = "";
+			int i = 3;
+			fileName = "DataStructure.xlsx";
 			/** Creating Input Stream**/  
 		    FileInputStream myInput = new FileInputStream(fileName);  
 		   /** Create a POIFSFileSystem object**/  
-		   POIFSFileSystem myFileSystem = new POIFSFileSystem(myInput);  
+		   //POIFSFileSystem myFileSystem = new POIFSFileSystem(myInput);  
 		   /** Create a workbook using the File System**/  
-		   HSSFWorkbook myWorkBook = new HSSFWorkbook(myFileSystem);  
+		   XSSFWorkbook myWorkBook = new XSSFWorkbook(myInput);  
 		    /** Get the first sheet from workbook**/  
-		   HSSFSheet mySheet = myWorkBook.getSheetAt(0);
+		   XSSFSheet mySheet = myWorkBook.getSheetAt(0);
 		   /** We now need something to iterate through the cells.**/  	   
 			// Reads the data in excel file until last row is encountered
 			while (i <= mySheet.getLastRowNum()) {
 				// Creates an object for the UserInfo Model
 				HealthMetrics healthMetric = new HealthMetrics();
 				// Creates an object representing a single row in excel
-				HSSFRow row = mySheet.getRow(i++);
+				XSSFRow row = mySheet.getRow(i++);
 				// Sets the Read data to the model class				
-				healthMetric.setHospitalName(row.getCell(1).getStringCellValue());
-				healthMetric.setTimeStamp(row.getCell(2).getStringCellValue());
-				healthMetric.setAadhar(row.getCell(3).getStringCellValue());
-				healthMetric.setGender(row.getCell(4).getStringCellValue());
-				healthMetric.setAge(row.getCell(5).getStringCellValue());
-				healthMetric.setIllness(row.getCell(6).getStringCellValue());
-				healthMetric.setState(row.getCell(7).getStringCellValue());
-				healthMetric.setLatLong(row.getCell(8).getStringCellValue());				
+				healthMetric.setHospitalName(row.getCell(0).toString());
+				healthMetric.setTimeStamp(row.getCell(1).toString());
+				healthMetric.setAadhar(row.getCell(2).toString());
+				healthMetric.setGender(row.getCell(3).toString());
+				healthMetric.setAge(row.getCell(4).toString());
+				healthMetric.setIllness(row.getCell(5).toString());
+				healthMetric.setState(row.getCell(6).toString());
+				healthMetric.setLatLong(row.getCell(7).toString());				
 				// persist data into database in here
 				healthMetricsRepo.save(healthMetric);
 			}								
